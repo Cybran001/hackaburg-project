1913808341f097f8c487507d93e04cf6ae33bc5b
Nikola Bergmaier <nikola.bergmaier@st.oth-regensburg.de>
2025-05-23 17:40:01 +0200

    added pausing for selling gear in timeline

--- cutscene_1.gd
+++ cutscene_1.gd
@@ -5,6 +5,7 @@ extends Control
 
 func _ready():
 	fade_in_character(char1)
+	Dialogic.signal_event.connect(_on_dialogic_signal_event)
 	Dialogic.start("timeline1")  # replace with your actual timeline name
 	Dialogic.timeline_ended.connect(_on_timeline1_end)
 
@@ -17,3 +18,13 @@ func _on_timeline1_end():
 	#fade_in_character(char2)
 	pass
 	#Dialogic.start("Timeline2")  # replace with your actual second timeline
+
+
+func _on_dialogic_signal_event(signal_name: String):
+	match signal_name:
+		"sell_gear":
+			Dialogic.paused = true
+			print("Selling gear...")
+			await get_tree().create_timer(5.0).timeout
+			print("Sold gear!!!")
+			Dialogic.paused = false
