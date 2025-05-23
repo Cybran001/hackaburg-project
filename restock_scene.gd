07090a308344e55df6ffd32069484ffa5c1d7767
Tobias Brunner <50907345+tobibrunner@users.noreply.github.com>
2025-05-23 17:44:23 +0200

    add can't buy when no money

--- restock_scene.gd
+++ restock_scene.gd
@@ -5,9 +5,14 @@ func _on_available_items_list_item_selected(index: int) -> void:
 	var selected_item_name = $AvailableItemsList.get_item_text(index)
 	var selected_item = Global.available_items[selected_item_name]
 
-	Global.remove_item_from_itemlist($AvailableItemsList, index)
-	Global.add_item_to_itemlist($SelectedItemsList, selected_item)
 
+	if selected_item.item_value <= Global.money_counter:
+		Global.remove_item_from_itemlist($AvailableItemsList, index)
+		Global.add_item_to_itemlist($SelectedItemsList, selected_item)
+
+		Global.money_counter -= selected_item.item_value
+	else:
+		print("todo no money")
 
 func _on_selected_items_list_item_selected(index: int) -> void:
 	var selected_item_name = $SelectedItemsList.get_item_text(index)
@@ -15,3 +20,4 @@ func _on_selected_items_list_item_selected(index: int) -> void:
 
 	Global.remove_item_from_itemlist($SelectedItemsList, index)
 	Global.add_item_to_itemlist($AvailableItemsList, selected_item)
+	Global.money_counter += selected_item.item_value
