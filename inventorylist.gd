0a8a76f591987d4a3581933da210c5337b488f23
Lang, Simon <simon.lang@vector.com>
2025-05-23 16:51:27 +0200

    Added coin icon and changed pos of inventory

--- inventorylist.gd
+++ inventorylist.gd
@@ -5,7 +5,12 @@ func _init():
 	add_item("Name", null, false)
 	add_item("Value", null, false)
 	
+	# Add coin texture and resize to 32x32
+	var _coin_texture = load("res://icons/coin.png").get_image()
+	_coin_texture.resize(32,32)
+	var coin_texture := ImageTexture.create_from_image(_coin_texture)
+	
 	# Add items in inventory
 	for item in Global.current_inventory:
 		add_item(item.item_name, item.item_texture)
-		add_item(str(item.item_value), null, false)
+		add_item(str(item.item_value), coin_texture, false)
