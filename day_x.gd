6b8c0310eb3fa227bf66675e1692b90ad4611f87
Nikola Bergmaier <nikola.bergmaier@st.oth-regensburg.de>
2025-05-23 20:07:23 +0200

    auto-face-out-day x

--- day_x.gd
+++ day_x.gd
@@ -6,6 +6,9 @@ func _ready() -> void:
 	Global.day += 1
 	dayX.text = "Day " + str(Global.day)
 	Global.fade_in(dayX)
+	await get_tree().create_timer(3.0).timeout
+	await Global.fade_out(dayX).finished
+	get_tree().change_scene_to_file("res://RestockScene.tscn")
 	
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
